ecc5ba83d7c8b6fd9aa2babde10f911e76f16b6d
Clement Ng <clementng23@hotmail.com>
2016-07-19 02:22:17 +1000

    added asura beido to db. added stuff to cog

--- cogs/bopae.py
+++ cogs/bopae.py
@@ -23,21 +23,42 @@ class Bopae:
     @commands.command()
     async def bopae(self, *text):
         """... because google sheets takes too long to load ;_;
-        Usage: !bopae [setAbbrev] [(1-8)]"""
+        Usage: !bopae list\n       !bopae [name]\n       !bopae [name] [1-8]"""
         
-        # if text == () or len(text) > 2:
+        if text == () or len(text) > 2:
             # await self.bot.say("Usage: !bopae [setAbbrev] [slotPos (1-8) (optional)]")
         # elif text[0] == "-user":
             # await self.bot.say("FEATURE UNIMPLEMENTED.")
             
-        if text == () or len(text) != 2:
-            await self.bot.say("Usage: !bopae [setAbbrev] [(1-8)]")
+        # if text == () or len(text) != 2:
+            await self.bot.say("`Usage: !bopae list\n       !bopae [name]\n       !bopae [name] [1-8]`")
         else:
-            # if len(text) == 1:
-                # """bopae set stats"""
+            if len(text) == 1:
+                """bopae set stats"""
                 # await self.bot.say("Feature unimplemented")
-                
-            # elif len(text) == 2:
+                if text[0] == "list":
+                    multiline = "Available SS sets: "
+                    for set in self.bopaeData:
+                        multiline += "{} ".format(set)
+ 
+                    await self.bot.say(multiline)
+                else:
+                    try:
+                        reqBopae = self.bopaeData[text[0]]
+                        multiline = ("```REQUESTED SOUL SHIELD SET [{}]\n"
+                            "set name: {}\n"
+                            "set notes: {}\n").format(text[0], reqBopae["setName"], reqBopae["setNotes"])
+                            
+                        for i in reqBopae["setBonus"]:
+                            multiline += "set bonus {}: {}\n".format(i, reqBopae["setBonus"][i])
+                        
+                        multiline += "```"
+                        await self.bot.say(multiline)
+                    except:
+                        await self.bot.say("invalid bopae request [{}]".format(text[0]))
+                    
+                    
+            elif len(text) == 2:
                 """specific bopae stat"""
                 try:
                     reqBopae = self.bopaeData[text[0]]["slot"+text[1]]
